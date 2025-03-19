b68cf0c06af46acb5ef719c20194b57cabd37a90
Lauri <lmt.jaakkola@gmail.com>
2025-03-19 17:55:30 +0200

    Added win condition logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 import { useState } from "react"
+import { WINNING_COMBINATIONS } from './winning-combinations'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
 
@@ -14,10 +21,32 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
+
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+for(const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row] [col] = player;
+}
+
+let winner;
+for (const combination of WINNING_COMBINATIONS) {
+  const firstSquareSymbol = gameBoard[combination[0].row] [combination[0].column]
+  const secondSquareSymbol = gameBoard[combination[1].row] [combination[1].column]
+  const thirdSquareSymbol = gameBoard[combination[2].row] [combination[2].column]
+
+   if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+    winner = firstSquareSymbol;
+  }
+}
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -38,7 +67,8 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} turns={gameTurns}/>
+        {winner && <p>You won, {winner}</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} board={gameBoard}/>
       </div>
       <Log turns={gameTurns} />
     </main>
