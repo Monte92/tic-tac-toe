0ed592f5a1030a01efbe4ea149f6e8e47e3fa0f0
Lauri <lmt.jaakkola@gmail.com>
2025-03-19 15:40:11 +0200

    Simplified the state handling

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-
-
 
 const initialGameBoard = [
     [null, null, null],
@@ -8,9 +5,9 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+export default function GameBoard({ onSelectSquare, turns }) {
 
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+/*     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
@@ -19,6 +16,15 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
             return updatedBoard;
         });
         onSelectSquare();
+    } */
+
+    let gameBoard = initialGameBoard;
+
+    for(const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row] [col] = player;
     }
 
 
@@ -29,7 +35,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                         <li key={colIndex}>
-                            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                         </li>
                         ))}
                 </ol>
